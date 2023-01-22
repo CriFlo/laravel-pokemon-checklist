f698d5e864f635bc6b321f57c7be854492b9b916
DevFlo <devflofe@gmail.com>
2023-01-23 00:01:55 +0100

    tests pwa

--- vite.config.js
+++ vite.config.js
@@ -19,10 +19,42 @@ export default defineConfig({
             },
         }),
         VitePWA({
+            useCredentials: true,
+            injectRegister: 'auto',
             registerType: 'autoUpdate',
             devOptions: {
                 enabled: true,
             },
+            manifest: {
+                name: "PokemonCheckList",
+                short_name: "PokemonCheckList",
+                description: "PokemonCheckList",
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                icons: [
+                    {
+                        src: '/storage/generic/192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png',
+                    },
+                    {
+                        src: '/storage/generic/512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                    }
+                ],
+                start_url: '/build/',
+                scope: '/build/',
+                // display: 'standalone',
+                // lang: 'en',
+            },
+            strategies: 'generateSW',
+            serviceWorker: {
+                output: 'sw.js',
+                registrationStrategy: 'registerImmediately',
+                registerType: 'autoUpdate',
+                registration: '/build/sw.js',
+            }//     'laravel-vite-plugin-manifest.css',
         }),
     ],
 });
